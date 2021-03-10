0aa721055e854c3c3a518a8e2a8cfa67ed887a7b
Hot Tamale <zweakland@setonhill.edu>
2021-03-09 23:49:28 -0500

    final version

--- InClassAlerts/ViewController.swift
+++ InClassAlerts/ViewController.swift
@@ -15,5 +15,32 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        // calling the alert method
+        showAlert()
+        // calling the actionsheet method
+        // showActionSheet()
+    }
+    
+    func showAlert(){
+        // create the alert object
+        let alert = UIAlertController(title: "Important Update", message: "Would you like to update your version?", preferredStyle: .alert)
+        //add a Dismiss button to our alert
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: {action in print("Pressed Dismiss")}))
+        
+        //present it to the screen
+        present(alert, animated: true)
+        
+    }
+    
+    func showActionSheet(){
+        // create action sheet object
+        let actionSheet = UIAlertController(title: "Important Update", message: "Would you like to update your version?", preferredStyle: .actionSheet)
+        // add a Dismiss button to our actionSheet
+        actionSheet.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: {action in print("Pressed Dismiss")}))
+        // show actionsheet on screen
+        present(actionSheet, animated: true)
+    }
+    
 }
 
